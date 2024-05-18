a0d7023c18fdf8c2b0f9c9f809915176ff257eda
NIK-GET <167620685+NIK-GET@users.noreply.github.com>
2024-05-18 15:35:39 +0300

    Update PIN_konk.py
    v2.2

--- PIN_konk.py
+++ PIN_konk.py
@@ -1,120 +1,134 @@
 from pygame import *
+'''Необходимые классы'''
 
-'''Необхадимые классы'''
-
-#игровая сцена
-back = (200, 255, 255) #цвет фона
-win_width = 600
-win_heaight = 500
-window = display.set_mode((win_width, win_heaight))
-window.fill(back) # fill заменить на blit для картинки
+# подгружаем отдельно функции для работы со шрифтом
+font.init()
+font1 = font.SysFont('Arial', 30)
 
 
-# нам нужны такие картинки:
-#img_back = "background.jpg" # фон игры
-img_racket = "racket.png" #ракетка
-img_ball = "tenis_ball.png" # мяч
+score1 = 0
+score2 = 0
 
-#флаги отвечающие за состаяние игры
+#фоновая музыка
+mixer.init()
+mixer.music.load('likovanie-publiki-27488.ogg')
+mixer.music.play()
+fire_sound = mixer.Sound('otskok-myacha.ogg')
+
+
+
+#класс-родитель для спрайтов
+class GameSprite(sprite.Sprite):
+   def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+       super().__init__()
+       self.image = transform.scale(image.load(player_image), (wight, height)) #вместе 55,55 - параметры
+       self.speed = player_speed
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
+
+
+   def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+class Player(GameSprite):
+   def update_r(self):
+       keys = key.get_pressed()
+       if keys[K_UP] and self.rect.y > 5:
+           self.rect.y -= self.speed
+       if keys[K_DOWN] and self.rect.y < win_height - 80:
+           self.rect.y += self.speed
+   def update_l(self):
+       keys = key.get_pressed()
+       if keys[K_w] and self.rect.y > 5:
+           self.rect.y -= self.speed
+       if keys[K_s] and self.rect.y < win_height - 80:
+           self.rect.y += self.speed
+
+
+#игровая сцена:
+
+
+win_width = 700
+win_height = 500
+display.set_caption(":)")
+window = display.set_mode((win_width, win_height))
+background = transform.scale(image.load('kort.png'), (win_width, win_height))
+
+
+#флаги, отвечающие за состояние игры
 game = True
 finish = False
-
-#таймер
 clock = time.Clock()
-FPS = 60
-
-while game:
-    for e in event.get():
-        if e.type == QUIT:
-            game = False
-
-    display.update()
-    clock.tick(FPS)
-
-class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y, player_speed, size_x, size_y):
-        super().__init__()
-        self.image = transform.scale(image.load(player_image), (size_x, size_y))
-        self.speed = player_speed
-        self.rect = self.image.get_rect()
-        self.rect.x = player_x
-        self.rect.y = player_y
-        
-
-    def reset(self):
-        window.blit(self.image, (self.rect.x, self.rect.y))
+FPS = 120
 
 
-# класс главных игроков
-class Player(GameSprite):
-    # метод для управления спрайтом стрелками клавиатуры
-    def update_r(self):
-        keys = key.get_pressed()
-        if keys[K_UP] and self.rect.y > 5:
-            self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.y < win_width - 80:
-            self.rect.y += self.speed
+#создания мяча и ракетки   
+racket1 = Player('racket.png', 30, 200, 4, 100, 150) 
+racket2 = Player('racket.png', 520, 200, 4, 100, 150)
+ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
 
-    def update_l(self):
-        keys = key.get_pressed()
-        if keys[K_W] and self.rect.y > 5:
-            self.rect.y -= self.speed
-        if keys[K_S] and self.rect.y < win_width - 80:
-            self.rect.y += self.speed
-
-
-racket1 = Player(img_racket, 30, 200, 4, 50, 150)  
-racket2 = Player(img_racket, 520, 200, 4, 50, 150) 
-ball = GameSprite(img_ball, 200, 200, 4, 50, 50)
 
 font.init()
-font1 = font.SysFont('Arial', 35)
-lose2 = font1.render('1 игрока на расстрел', True, (180, 0, 0))
-lose1 = font1.render('2 игрока на расстрел', True, (180, 0, 0))
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
+
+
 
 speed_x = 3
 speed_y = 3
 
 
 while game:
-    # событие нажатия на кнопку Закрыть
-    for e in event.get():
-        if e.type == QUIT:
-            game = False
+   for e in event.get():
+       if e.type == QUIT:
+           game = False
+  
+   if finish != True:
 
-    if finish != True:
-        window.fill(back)
-        racket1.update_l()
-        racket2.update_r()
-        ball.rect.x += speed_x
-        ball.rect.y += speed_y
 
-# если мяч достиг границ экранна то он меняет направление его движения
-        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+       window.blit(background, (0,0))
+       racket1.update_l()
+       racket2.update_r()
+       ball.rect.x += speed_x
+       ball.rect.y += speed_y
+
+       text1 = font1.render("Счет: " + str(score1), 1, (255, 25, 255))
+       window.blit(text1, (30, 90))
+       text2 = font1.render("Счет: " + str(score2), 1, (255, 25, 255)) 
+       window.blit(text2, (580, 90))  
+
+
+       if sprite.collide_rect(racket1, ball): 
+           speed_x *= -1
+           speed_y *= 1
+           score1 = score1 + 1
+
+
+       if sprite.collide_rect(racket2, ball):
             speed_x *= -1
             speed_y *= 1
-        if ball.rect.y > win_heaight-50 or ball.rect.y < 0:
-            speed_y *= -1
-
-# если мяч улетел за ракетки, выводим надпись проигрыша для первого игрока
-        if ball.rect.x < 0:
-            finish = True
-            window.blit(lose1, (200, 200))
-            game_over = True
-
-# если мяч улетел за ракетки, выводим надпись проигрыша для второго игрока
-        if ball.rect.x > win_width:
-            finish = True
-            window.blit(lose2, (200, 200))
-            game_over = True
+            score2 = score2 + 1
+      
+       #если мяч достигает границ экрана, меняем направление его движения
+       if ball.rect.y > win_height-50 or ball.rect.y < 0:
+           speed_y *= -1
 
 
-        racket1.reset()
-        racket2.reset()
-        ball.reset()
-
-            
-    display.update()
-    clock.tick(FPS)
+       #если мяч улетел дальше ракетки, выводим условие проигрыша для первого игрока
+       if ball.rect.x < 0:
+           finish = True
+           window.blit(lose1, (250, 90))
+           game_over = True
 
 
+       #если мяч улетел дальше ракетки, выводим условие проигрыша для второго игрока
+       if ball.rect.x > win_width:
+           finish = True
+           window.blit(lose2, (250, 90))
+           game_over = True
+        
+
+   
